dd65a37914e82392418bcd586d1a703306ec3217
Kyle McConaughey <kylemcconaughey03@gmail.com>
2020-11-06 16:02:16 -0500

    User fields are nullable for now

--- users/models.py
+++ users/models.py
@@ -4,23 +4,23 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
 
-    first_name = models.CharField(null=False, blank=False, max_length=63)
+    first_name = models.CharField(null=True, blank=True, max_length=63)
 
-    last_name = models.CharField(null=False, blank=False, max_length=63)
+    last_name = models.CharField(null=True, blank=True, max_length=63)
 
-    num_pets = models.IntegerField(null=False, blank=False, default=0)
+    num_pets = models.IntegerField(null=True, blank=True, default=0)
 
-    street_address = models.CharField(null=False, blank=False, max_length=255)
+    street_address = models.CharField(null=True, blank=True, max_length=255)
 
-    city = models.CharField(null=False, blank=False, max_length=63)
+    city = models.CharField(null=True, blank=True, max_length=63)
 
-    state = models.CharField(null=False, blank=False, max_length=63)
+    state = models.CharField(null=True, blank=True, max_length=63)
 
     created_at = models.DateTimeField(auto_now_add=True)
 
-    phone_num = models.IntegerField(null=False, blank=False)
+    phone_num = models.IntegerField(null=True, blank=True)
 
-    birthdate = models.DateField(null=False, blank=False)
+    birthdate = models.DateField(null=True, blank=True)
     # should have a minimum age/maximum date such that min. age = 18
 
     profile_picture = models.ImageField(upload_to="post_images/", null=True, blank=True)
