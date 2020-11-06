0e32218c556cd132d6be708a5d91ccc2451c776f
Kyle McConaughey <kylemcconaughey03@gmail.com>
2020-11-06 17:27:11 -0500

    s3 image hosting

--- project/settings.py
+++ project/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     "djoser",
     "corsheaders",
     "rest_framework.authtoken",
+    "storages",
     # Project-specific
     "users",
     "api",
@@ -156,7 +157,26 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
-    ]
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+MEDIA_URL = "/media/"
+MEDIA_DIR = BASE_DIR / "media"
+
+
+# Amazon s3 Settings
+AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+AWS_S3_OBJECT_PARAMETERS = {
+    "CacheControl": "max-age=86400",
+}
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = "public-read"
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
