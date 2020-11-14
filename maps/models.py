ecedd70b9b367c99f9062cb5dae477e64bf98b2e
Kyle McConaughey <kylemcconaughey03@gmail.com>
2020-11-14 16:28:40 -0500

    maps/models += Location

--- maps/models.py
+++ maps/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from mapbox_location_field.spatial.models import SpatialLocationField
 
-# Create your models here.
+
+class Location(models.Model):
+    location = SpatialLocationField(
+        map_attrs={
+            "style": "mapbox://styles/mapbox/streets-v11",
+            "center": (-80.793457, 35.782169),
+            "zoom": 5,
+        }
+    )
