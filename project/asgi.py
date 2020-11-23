d8a104ab6b29c84d258cfcdcff3ea70044123ad0
Kyle McConaughey <kylemcconaughey03@gmail.com>
2020-11-23 13:35:54 -0500

    asgi setup

--- project/asgi.py
+++ project/asgi.py
@@ -8,13 +8,12 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
 import os
+import django
 
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import get_default_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
-# application = get_asgi_application()
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-})
+django.setup()
+
+application = get_default_application()
