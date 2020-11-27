aa93b50f2c8bab58e78aa04527db132373722dd6
Kyle McConaughey <kylemcconaughey03@gmail.com>
2020-11-27 14:31:53 -0500

    reorganized API url groupings

--- api/urls.py
+++ api/urls.py
@@ -13,19 +13,19 @@ api_router.register(
 )
 api_router.register("messages", api_views.MessageViewSet, basename="message")
 api_router.register("meetups", api_views.MeetupViewSet, basename="meetup")
-api_router.register("reactions", api_views.ReactionViewSet, basename="reaction")
+api_router.register("locations", api_views.LocationViewSet, basename="location")
 api_router.register("posts", api_views.PostViewSet, basename="post")
 api_router.register("comments", api_views.CommentViewSet, basename="comment")
-api_router.register("requests", api_views.RequestViewSet, basename="request")
 api_router.register(
     "discussionboards", api_views.DiscussionBoardViewSet, basename="discussionboard"
 )
-api_router.register("locations", api_views.LocationViewSet, basename="location")
 api_router.register("notes", api_views.NoteViewSet, basename="note")
 api_router.register("websockets", api_views.SocketViewSet, basename="websocket")
 api_router.register(
     "notifications", api_views.NotificationViewSet, basename="notifications"
 )
+api_router.register("requests", api_views.RequestViewSet, basename="request")
+api_router.register("reactions", api_views.ReactionViewSet, basename="reaction")
 
 
 urlpatterns = [
